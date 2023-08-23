c611b1145e2bc673d78aad2e05dbe90815484432
Colin Pfeiffer <colin.pfeiffer@web.de>
2023-08-23 10:14:44 +0200

    added basic code for invader (23.08.2023)

---
+++ invader_os.py
@@ -0,0 +1,38 @@
+import os
+from time import sleep
+import pygame
+
+
+class Settings:
+    WINDOW_WIDTH = 600
+    WINDOW_HEIGHT = 400
+    FPS = 60
+
+
+def main():
+    os.environ["SDL_VIDEO_WINDOW_POS"] = "10, 50"
+    pygame.init()
+
+    screen = pygame.display.set_mode((Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT))
+    pygame.display.set_caption("Bitmaps laden und ausgeben")
+    clock = pygame.time.Clock()
+
+    defender = pygame.image.load("images/defender01.png")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("white")
+        screen.blit(defender, (100, 100))
+
+        pygame.display.flip()
+        clock.tick(Settings.FPS)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
