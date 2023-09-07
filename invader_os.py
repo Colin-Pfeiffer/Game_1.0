afc79fc70960ceee53ffada28da94e19d10154fa
Colin Pfeiffer <colin.pfeiffer@web.de>
2023-09-07 10:38:27 +0200

    added Sprite Classes & simplified code

--- invader_os.py
+++ invader_os.py
@@ -4,28 +4,59 @@ import pygame
 
 
 class Settings:
-    WINDOW_WIDTH = 600
-    WINDOW_HEIGHT = 400
+    WINDOW = pygame.rect.Rect(0, 0, 600, 400)
     FPS = 60
     FILE_PATH = os.path.dirname(os.path.abspath(__file__))
     IMAGE_PATH = os.path.join(FILE_PATH, "images")
 
+class Defender(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "defender01.png")).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = Settings.WINDOW.centerx
+        self.rect.bottom = Settings.WINDOW.bottom
+        self.speed_x = 10
+        self.speed_y = 0
+
+    def update(self):
+        self.rect.move_ip(self.speed_x, self.speed_y)
+        if self.rect.right > Settings.WINDOW.right or self.rect.left <= 0:
+            self.speed_x *= -1
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "alienbig0101.png")).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = Settings.WINDOW.centerx
+        self.rect.top = Settings.WINDOW.top
+        self.speed_x = 10
+        self.speed_y = 0
+
+    def update(self):
+        self.rect.move_ip(self.speed_x, self.speed_y)
+        if self.rect.right > Settings.WINDOW.right or self.rect.left <= 0:
+            self.speed_x *= -1
+            self.rect.move_ip(0, self.rect.height)
+            
 
 def main():
     os.environ["SDL_VIDEO_WINDOW_POS"] = "10, 50"
     pygame.init()
 
-    screen = pygame.display.set_mode((Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT))
+    screen = pygame.display.set_mode((Settings.WINDOW.size))
     pygame.display.set_caption("Bitmaps laden und ausgeben")
     clock = pygame.time.Clock()
 
-    background = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "sunset.png")).convert_alpha()
+    background_image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "sunset.png")).convert_alpha()
+    background_image = pygame.transform.scale(background_image, Settings.WINDOW.size)
 
-    defender = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "defender01.png")).convert_alpha()
-    defender = pygame.transform.scale(defender, (50, 50))
+    defender = Defender()
 
-    alien = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "alienbig0101.png")).convert_alpha()
-    alien = pygame.transform.scale(alien, (110, 50))
+    alien = Alien()
 
     running = True
     while running:
@@ -33,10 +64,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill("white")
-        screen.blit(background, (0, 0))
-        screen.blit(defender, (100, 100))
-        screen.blit(alien, (400, 100))
+        defender.update()        
+        alien.update()
+
+        screen.blit(background_image, (0, 0))
+        screen.blit(defender.image, defender.rect)
+        screen.blit(alien.image, alien.rect)
 
         pygame.display.flip()
         clock.tick(Settings.FPS)
